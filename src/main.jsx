9bad517633d7d5d333cc783de3eb21290a7fa1ed
Luciano Garriga <lucianomgarriga@gmail.com>
2023-08-01 20:57:48 -0300

    Create States - Reducers/Functions - Actions

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import Config Function of App Store
+import {createAppStore} from './store/config/storeConfig';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
